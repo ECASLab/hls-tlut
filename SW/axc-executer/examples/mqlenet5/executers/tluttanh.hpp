933dcb89a6ab2d71e7596a7564a757b0692fb800
SP <sporras29@estudiantec.cr>
2026-05-22 03:57:29 -0600

    Update meson and api path

--- SW/axc-executer/examples/mqlenet5/executers/tluttanh.hpp
+++ SW/axc-executer/examples/mqlenet5/executers/tluttanh.hpp
@@ -21,7 +21,7 @@
 #include <layer.hpp>
 #include <runtime.hpp>
 
-#include "../../../api/tlut_api.hpp" // t-LUT Accelerator API
+#include "../../../../api/tlut_api.hpp" // t-LUT Accelerator API
 
 inline TlutAccelerator& get_tlut_accelerator() {
   static TlutAccelerator accel("../HW/package.hw.v2/kernels_v2.xclbin", 6, 10);
