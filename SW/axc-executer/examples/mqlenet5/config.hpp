3c7f42c7316cbf6ee13db9ea3e4e6d15ce66f3ed
SP <sporras29@estudiantec.cr>
2026-05-30 17:55:53 -0600

    update

--- SW/axc-executer/examples/mqlenet5/config.hpp
+++ SW/axc-executer/examples/mqlenet5/config.hpp
@@ -36,7 +36,7 @@ static constexpr std::pair<int, int> kQT[] = {
     {16, 6},                    /* Add 1D */
     {Q_DENSE_BW, Q_DENSE_INT},   /* Dense */
     {Q_ADD1D_BW, Q_ADD1D_INT},  /* Add 1D */
-    {20, 4}                     /* Softmax */
+    {16, 6}                     /* Softmax */
 };
 
 /**
